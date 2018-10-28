7742b1b4e7ed99c82e93091b786ec9d768480216
RubenAMtz <ruben.amtz@outlook.com>
2018-10-28 21:40:04 +0000

    Refactored code

--- stochastic_gradient_descent_classifier.py
+++ stochastic_gradient_descent_classifier.py
@@ -4,15 +4,17 @@ from sklearn import metrics, preprocessing, linear_model, tree, svm
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import model_selection
+from sklearn.externals import joblib
+
 
 def main():
     # Set seed for reproducibility
     seed = np.random.seed(42)
     print("# Loading data...")
-    train = pd.read_csv('numerai_training_data.csv', header=0)
+    train = pd.read_csv('../numerai_training_data.csv', header=0)
     #data from data_engineering process
-    selected_features = pd.read_csv('x_new.csv', header=0)
-    tournament = pd.read_csv('numerai_tournament_data.csv', header=0)
+    selected_features = pd.read_csv('../x_new.csv', header=0)
+    tournament = pd.read_csv('../numerai_tournament_data.csv', header=0)
     validation = tournament[tournament['data_type']=='validation']
     train_bernie = train
 
@@ -26,131 +28,60 @@ def main():
 
     #CONFIGURE YOUR MODELS:
     modelSGDC = linear_model.SGDClassifier(loss='log', max_iter=1000, tol=0.001)
-    print("# Training...")
-    modelSGDC.fit(X, Y)
-
-    #Stochastic Gradient Boosting Classification
-    num_trees = 100
-    kfold = model_selection.KFold(n_splits=len(train['era'].unique()), random_state=seed)
-    #Configure model
-    modelGBC = GradientBoostingClassifier(n_estimators=num_trees, random_state=seed, verbose=2)
-    #Train and test with kfold model iterations
-    results = model_selection.cross_val_score(modelGBC, X, Y, cv=kfold)
-    #print(results.mean())
     
-    #COMMENT IF YOU DON'T WANT TO SAVE THE TRAINED MODEL
-    joblib.dump(modelGBC, 'gradient_classifier.joblib') 
+    #APPLY A CROSS-VALIDATION STRATEGY, SET SCORING TO A COMMON METRIC = MEAN SQUARED ERROR. DEFINE 10 FOLDS
+    print("# Cross-Validating...\n")
+    scores = cross_val_score(modelSGDC, X, Y, scoring="neg_mean_squared_error", cv=10)
+    rmse_scores = np.sqrt(-scores)
+    print("Scores:",  rmse_scores)
+    print("Mean:",  rmse_scores.mean())
+    print("Standard deviation:",  rmse_scores.std())
+
+    print("# Training...")
+    modelSGDC.fit(X,Y)
     #UNCOMMENT IF WANT TO LOAD THE TRAINED MODEL
     # modelGBC = joblib.load('gradient_classifier.joblib')
-    modelGBC.fit(X,Y)  
-
-
-    # modelKNN = KNeighborsClassifier(n_neighbors=3)
-    # print("# Training...")
-    # modelKNN.fit(X, Y)
-
-    #APPLY A CROSS-VALIDATION STRATEGY, SET SCORING TO A COMMON METRIC = MEAN SQUARED ERROR. DEFINE 10 FOLDS
-    # print("# Predicting...MODEL ExtraTreeClassifier")
-    # scores = cross_val_score(modelETC, X, Y, scoring="neg_mean_squared_error", cv=10)
-    # rmse_scores = np.sqrt(-scores)
-    # print("Scores:",  rmse_scores)
-    # print("Mean:",  rmse_scores.mean())
-    # print("Standard deviation:",  rmse_scores.std())
-
-        # print("# Predicting...MODEL Stochastic Gradient Descent Classifier")
-        # scores = cross_val_score(modelSGDC, X, Y, scoring="neg_mean_squared_error", cv=10)
-        # rmse_scores = np.sqrt(-scores)
-        # print("Scores:",  rmse_scores)
-        # print("Mean:",  rmse_scores.mean())
-        # print("Standard deviation:", rmse_scores.std())
-
-    # print("# Predicting...MODEL K Neighbors Classifier")
-    # scores = cross_val_score(modelKNN, X, Y, scoring="neg_mean_squared_error", cv=10)
-    # rmse_scores = np.sqrt(-scores)
-    # print("Scores:",  rmse_scores)
-    # print("Mean:",  rmse_scores.mean())
-    # print("Standard deviation:",  rmse_scores.std())
+    #COMMENT IF YOU DON'T WANT TO SAVE THE TRAINED MODEL
+    joblib.dump(modelSGDC, 'sgradientd_classifier.joblib') 
     
-    #USED TRAINED MODELS AND TEST THEM AGAINST THE TEST SET (x_prediction is the validation set)
-    #print(".")
-    # y_prediction = modelETC.predict_proba(x_prediction)
-    # probabilities = y_prediction[:, 1]
-    # print(probabilities)
-    # print("- probabilities ETC:", probabilities[1:6])
-    # print("- target:", validation['target_bernie'][1:6])
-    # print("- rounded probability:", [round(p) for p in probabilities][1:6])
-    # correct = [round(x)==y for (x,y) in zip(probabilities, validation['target_bernie'])]
-    # print("- accuracy: ", sum(correct)/float(validation.shape[0]))
-    # print("- validation logloss:", metrics.log_loss(validation['target_bernie'], probabilities))
-    print(".")
-    print("--------")
-    print(".")
-    y_prediction = modelGBC.predict_proba(x_prediction)
+    #USE TRAINED MODEL AND TEST THEM AGAINST THE TEST SET (x_prediction is the validation set)
+    y_prediction = modelSGDC.predict_proba(x_prediction)
     probabilities = y_prediction[:, 1]
     print(probabilities)
     print("- probabilities SGDC:", probabilities[1:6])
-    print("- target:", validation['target_bernie'][1:6])
+    print("- target:\n", validation['target_bernie'][1:6])
     print("- rounded probability:", [round(p) for p in probabilities][1:6])
     correct = [round(x)==y for (x,y) in zip(probabilities, validation['target_bernie'])]
-    print("- accuracy: ", sum(correct)/float(validation.shape[0]))
+    print("- accuracy:", sum(correct)/float(validation.shape[0]))
     print("- validation logloss:", metrics.log_loss(validation['target_bernie'], probabilities))
-    print(".")
-    print("--------")
-    print(".")
-    # y_prediction = modelKNN.predict_proba(x_prediction)
-    # probabilities = y_prediction[:, 1]
-    # print(probabilities)
-    # print("- probabilities KNN:", probabilities[1:6])
-    # print("- target:", validation['target_bernie'][1:6])
-    # print("- rounded probability:", [round(p) for p in probabilities][1:6])
-    # correct = [round(x)==y for (x,y) in zip(probabilities, validation['target_bernie'])]
-    # print("- accuracy: ", sum(correct)/float(validation.shape[0]))
+    print("\n--------\n")
 
     # # The targets for each of the tournaments are very correlated.
-        # print("COMPARING AGINST OTHER TARGETS: ----")
-        # print("Correlation between targets\n")
-        # tournament_corr = np.corrcoef(validation['target_bernie'], validation['target_elizabeth'])
-        # print("- bernie vs elizabeth corr:", tournament_corr)
-        # # # You can see that target_elizabeth is accurate using the bernie model as well.
-        # print("\nRounded probabilities (from original target) vs Elizabeth Target\n")
-        # correct = [round(x)==y for (x,y) in zip(probabilities, validation['target_elizabeth'])]
-        # print("- elizabeth using bernie:", sum(correct)/float(validation.shape[0]))
-
-    # # Numerai measures models on logloss instead of accuracy. The lower the logloss the better.
-    # # Numerai only pays models with logloss < 0.693 on the live portion of the tournament data.
-    # # Our validation logloss isn't very good.
-    # print("- validation logloss:", metrics.log_loss(validation['target_bernie'], probabilities))
+    print("COMPARING AGAINST OTHER TARGETS:\n")
+    print("Correlation between targets\n")
+    tournament_corr = np.corrcoef(validation['target_bernie'], validation['target_elizabeth'])
+    print("- bernie vs elizabeth corr:\n", tournament_corr)
+    # # You can see that target_elizabeth is accurate using the bernie model as well.
+    print("\nRounded probabilities (from original target) vs Elizabeth Target\n")
+    correct = [round(x)==y for (x,y) in zip(probabilities, validation['target_elizabeth'])]
+    print("- elizabeth using bernie:", sum(correct)/float(validation.shape[0]))
 
     # To submit predictions from your model to Numerai, predict on the entire tournament data.
-    print(".")
-    print("PREDICTIONS FOR THE TOURNAMENT *******************")
-    print(".")
-
-        # x_prediction = tournament[features]
-    # print("PREDICTIONS USING TREE")
-    # y_prediction = modelETC.predict_proba(x_prediction)
-    # results = y_prediction[:, 1]
-    # results_ETC = pd.DataFrame(data={'probability':results})
-    # joined = pd.DataFrame(ids).join(results_ETC)
-    # print("- joined:", joined.head())
-
-        # print("\nPREDICTIONS USING SGDC")
-        # y_prediction = modelSGDC.predict_proba(x_prediction)
-        # results = y_prediction[:, 1]
-        # #results = np.round_(results)
-        # results_SGDC = pd.DataFrame(data={'probability_bernie':results})
-        # joined = pd.DataFrame(ids).join(results_SGDC)
-        # print("- joined:", joined.head())
-        # print("# Writing predictions to bernie_submissions.csv...")
-        # # Save the predictions out to a CSV file.
-        # # print("# Creating submission...")
-        # joined.to_csv("bernie_submission.csv", index=False)`
-
-
-    # # Now you can upload these predictions on https://numer.ai
+    print("\n******************* TOURNAMENT PREDICTIONS *******************\n")
+    x_prediction = tournament[features]
+    print("Predictions using SGDC Model")
+    y_prediction = modelSGDC.predict_proba(x_prediction)
+    results = y_prediction[:, 1]
+    results_SGDC = pd.DataFrame(data={'probability_bernie':results})
+    joined = pd.DataFrame(ids).join(results_SGDC)
+    print("- joined:\n", joined.head())
+    print("# Writing predictions to bernie_submissions.csv...")
+    # Save the predictions out to a CSV file.
+    joined.to_csv("bernie_submission.csv", index=False)
+    # # predictions on https://numer.ai
 
 """
-TIPS TO IMPROVE YOUR MODEL
+TIPS TO IMPROVE THE MODEL
 
 1. Use eras
 In this example, we dropped era column but you can use the era column to improve peformance across eras
